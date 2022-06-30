f262be8ddb67dea3d19d62d2816732b89c1c564f
micolCODES <micolromano124@gmail.com>
2022-06-30 12:48:50 +0200

    Created Pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-alert('Welcome');
+var pokemonList = [];
 
-let favoriteFood = "Sushi🍱";
-document.write(favoriteFood);
-
-// single line comment
-
-/* multiple line comment */
+pokemonList.push({name: "Charizard", height: 1.7,types: ["fire", "flying"]});
+pokemonList.unshift({name: "Squirtle", height: 0.5, types: ["water"]});
+pokemonList.push({name: "Bulbasaur",height: 0.7, types: ["grass", "poison"]});
+pokemonList.unshift({name: "Pikachu",height: 0.4, types: ["electric"]});
+pokemonList.push({name: "Nidoran",height: 0.5, types: ["poison"]});
