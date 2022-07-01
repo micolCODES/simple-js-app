5b29c3e09076005cda6bfe58b688f96a85214ffd
micolCODES <micolromano124@gmail.com>
2022-07-01 10:03:07 +0200

    * Add loop to print character names and heights

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@ pokemonList.unshift({name: "Squirtle", height: 0.5, types: ["water"]});
 pokemonList.push({name: "Bulbasaur",height: 0.7, types: ["grass", "poison"]});
 pokemonList.unshift({name: "Pikachu",height: 0.4, types: ["electric"]});
 pokemonList.push({name: "Nidoran",height: 0.5, types: ["poison"]});
+
+let height = ""
+for (let i =0; i < pokemonList.length; i++) {
+    document.write("<BR>" + pokemonList[i].name + ": (height: " + pokemonList[i].height + ")");
+    if (pokemonList[i].height >= 0.7) {
+        document.write(" - Wow, that's big!");
+    }
+}
